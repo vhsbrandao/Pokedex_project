a42f1e9f7a360b9562cfd6f2158ac092f8922e03
Victor Hugo <backupvictorspiritus@gmail.com>
2024-04-22 13:11:25 -0300

    pokemons listados

--- src/store/getters.js
+++ src/store/getters.js
@@ -6,10 +6,8 @@ export default {
     return state.list.map(info => ({
       id: info.id,
       name: info.name,
-      moves: info.moves.map(({ move }) => move.name),
       types: info.types.map(({ type }) => type.name),
-      EvolutionChain: info.EvolutionChain.map(({ chain }) => chain.name),
-      sprite: info.sprites.showdown,
+      sprite: info.sprites.front_shiny,
     }));
   },
 };
